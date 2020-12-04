a08cdb41b12ad4dd70661c514059feba006f93ec
nastenkapuse4ka <nbakhshieva333@gmail.com>
2020-12-04 19:20:26 +0300

    init

---
+++ src/main/java/com/nastenkapusechka/mypersonaldiary/MyPersonalDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.nastenkapusechka.mypersonaldiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPersonalDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPersonalDiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nastenkapusechka/mypersonaldiary/MyPersonalDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nastenkapusechka.mypersonaldiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPersonalDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
