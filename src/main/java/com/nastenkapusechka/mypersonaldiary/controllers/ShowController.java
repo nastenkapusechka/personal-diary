cc21bea9ee5f572446a9095d71b69333a6a893ff
nastenkapuse4ka <nbakhshieva333@gmail.com>
2020-12-07 23:46:06 +0300

    develop ShowController and views (move js func to html-page - list.html

--- src/main/java/com/nastenkapusechka/mypersonaldiary/controllers/ShowController.java
+++ src/main/java/com/nastenkapusechka/mypersonaldiary/controllers/ShowController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ShowController {
@@ -20,5 +21,17 @@ public class ShowController {
     public String getSecrets(Model model) {
         model.addAttribute("secrets", repository.findAll());
         return "list";
+    }
+
+    @GetMapping("/show/details/{id}")
+    public String getDetails(@PathVariable Long id, Model model) {
+        model.addAttribute("details", repository.findById(id).get());
+        return "secret-details";
+    }
+
+    @GetMapping("/show/delete/{id}")
+    public String deleteSecret(@PathVariable Long id) {
+        repository.deleteById(id);
+        return "redirect:/show";
     }
 }
